48b7dfb1e443e49b3ca8191231b544313d3e047b
hagishi <hagishi.c@gmail.com>
2015-06-19 18:08:30 +0900

    ci test

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -20,7 +20,7 @@ class MockPDOStatement extends PDOStatement {
        if ($this->current_row == 5) {
            return false;
        } else {
-           return array('name' => 'Fred', 'age' => 10, 'id' => ++$this->current_row);
+           return array('name' => 'Fred', 'age' => 11, 'id' => ++$this->current_row);
        }
    }
 }
